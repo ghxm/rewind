1bc9e4d5c901cd27eef3dff151663f2215e8db02
Maximilian Haag <max.haag@gmx.net>
2020-04-23 09:22:17 +0200

    revert  debugging, fix bug

--- utilities.py
+++ utilities.py
@@ -7,13 +7,12 @@ rewind_dir = pathlib.Path(__file__).parent.absolute()
 def get_config():
     global rewind_dir
     config = configparser.ConfigParser()
-    print(pathlib.PurePath(rewind_dir, 'config.ini'))
     config.read(pathlib.PurePath(rewind_dir, 'config.ini'))
     return config
 
 def connect_db():
     config = get_config()
-    return apsw.Connection(pathlib.PurePath(rewind_dir, config['DB']['db']))
+    return apsw.Connection(str(pathlib.PurePath(rewind_dir, config['DB']['db'])))
 
 def get_cursor():
     conn = connect_db()
