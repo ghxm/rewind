8629a6848d462f7876d81fbf9f2e50545faa4edd
Maximilian Haag <max.haag@gmx.net>
2020-04-25 11:54:28 +0200

    fix recording and scheduling

--- recording.py
+++ recording.py
@@ -2,29 +2,45 @@ import time
 import datetime
 import subprocess
 import utilities
-
+import re
+import pytz
 config = utilities.get_config()
 
 class Recording:
-    def __init__(self, stream_url, station_name, show_name, time_start, time_stop = None, show_length = None, split_tracks = False):
+    def __init__(self, stream_url, station_name, show_name, time_start = None, time_stop = None, show_length = None, split_tracks = False):
         # @TODO if stations is known automatically select stream url/fallback etc
         self.stream_url = stream_url
         self.station_name = station_name
         self.show_name = show_name
-        self.time_start = time_start
+        if time_start == None:
+            self.time_start = datetime.datetime.now(pytz.utc)
+        else:
+            self.time_start = time_start
         self.time_stop = time_stop
-        self.show_length = show_length # calculate if not given
+        if show_length is not None:
+            self.show_length = int(show_length) # calculate if not given
+            self.time_stop = self.time_start + datetime.timedelta(seconds = show_length)
+        else:
+            self.show_length = abs(self.time_start-self.time_stop).total_seconds()
+
         self.split_tracks = split_tracks
 
-        if self.show_length is None:
-            self.show_length = abs(self.time_start-self.time_stop).seconds
 
     def record(self):
         # wait for show to start
-        while abs(datetime.datetime.now(datetime.timezone.utc) - self.time_start).seconds < 30: # seconds before show
-            time.sleep(1)
+        if datetime.datetime.now(pytz.utc) < self.time_start: # if recording start is in the future
+            while abs(datetime.datetime.now(pytz.utc) - self.time_start).seconds > 30: # wait until 30 seconds before show
+                time.sleep(1)
+        # @TODO time and timezones
+        # @TODO recording does not work
+        # @TODO: start by testing streamripper directly on server with options set below
         # start ripping
-        return subprocess.Popen(f'{config.get("STREAMRIPPER", "path")} {self.stream_url} -d {config.get("STREAMRIPPER", "outdir")} -a %d_%S {"-A" if not self.split_tracks else ""} -l {self.show_length+30}', shell = True)
+        print("starting recording")
+        return subprocess.Popen(f'{config.get("STREAMRIPPER", "path")} '
+                                f'{self.stream_url} -d {config.get("STREAMRIPPER", "outdir")} '
+                                f'-a %d__{re.sub(" ", "_", self.station_name)}__{re.sub(" ", "_", self.show_name)} '
+                                f'{"-A" if not self.split_tracks else ""} -l {self.show_length+30}',
+                                shell = True)
 
 
 
--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,7 @@ import utilities
 import datetime
 from recording import Recording
 import time
+import pytz
 
 # connect to database
 cur = utilities.get_cursor()
@@ -11,16 +12,19 @@ cur = utilities.get_cursor()
 cur.execute('SELECT id, station_name, show_name, time_start, time_stop, stream_url FROM scheduled_recordings WHERE processed = 0')
 scheduled_recordings = [{'id': row[0], 'station_name': row[1], 'show_name': row[2], 'time_start': datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M %z'), 'time_stop': datetime.datetime.strptime(row[4], '%Y-%m-%d %H:%M %z'), 'stream_url': row[5]} for row in list(cur)]
 
-now = datetime.datetime.now(datetime.timezone.utc)
+now = datetime.datetime.now(pytz.utc)
 
 # if any recording scheduled within the next 10 minutes, create Recording object and start record()
 recordings=[]
 for rec in scheduled_recordings:
-    if abs(rec['time_start'] - now).seconds/60 < 100:
+    if rec['time_stop'] < now: # if show has already ended
+        continue
+    if rec['time_start'] < now or abs(rec['time_start'] - now).seconds < 100:
         id = rec['id']
         recordings.append(Recording(stream_url=rec['stream_url'], station_name=rec['station_name'], show_name=rec['show_name'], time_start=rec['time_start'], time_stop=rec['time_stop']).record())
         # Update entry with processed = True
         cur.execute (f'UPDATE scheduled_recordings SET processed = 1 WHERE id={id}')
+# wait until all reacordings are finished to keep streamripper process running
 while not all([rec.poll() is not None for rec in recordings]):
     time.sleep(1)
 
